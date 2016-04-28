bedeeac955484f313cfd4f07bdb999cd12c747ce
= <camerontaylor00@gmail.com>
2016-04-28 04:31:04 -0700

    Stable version of quickstart

--- debian_quickstart.sh
+++ debian_quickstart.sh
@@ -6,8 +6,6 @@ echo "Installing dependancies for Alan"
 apt-get install git
 apt-get install python-pip
 apt-get install festival
-apt-get install pocketsphinx-utils
-sudo apt-get install pocketsphinx-hmm-en-hub4wsj
 apt-get install libjack0 libjack-dev
 apt-get install portaudio19-dev
 apt-get install python-all-dev
@@ -24,5 +22,27 @@ apt-get install pyaudio
 apt-get install git
 apt-get install libopencv-dev python-opencv
 
+
+echo "Downloading dependencies for pocketsphinx"
+apt-get install autconf
+apt-get install bison
+apt-get install libtool
+apt-get install swig
+apt-get install automake
+apt-get install gcc
+git clone https://github.com/cmusphinx/sphinxbase
+git clone https://github.com/cmusphinx/pocketsphinx
+(cd sphinxbase && ./autogen.sh)
+(cd sphinxbase && ./configure)
+(cd sphinxbase && make)
+(cd sphinxbase && make install)
+export LD_LIBRARY_PATH=/usr/local/lib
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+(cd pocketsphinx && ./autogen.sh)
+(cd pocketsphinx && ./configure)
+(cd pocketsphinx && make)
+(cd pocketsphinx && make install)
+
 echo "Downloading alan from github"
 git clone https://github.com/camtaylor/alan.git
+(cd alan && bash passive_listening.sh)
