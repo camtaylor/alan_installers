409c452e26f0fe41936f6623f4cd3378e8e6fc72
Cameron <camerontaylor00@gmail.com>
2016-04-27 23:53:13 -0700

    Removed sudo

--- debian_quickstart.sh
+++ debian_quickstart.sh
@@ -2,22 +2,22 @@
 
 # written by @github user gameslayer
 echo "Installing dependancies for Alan"
-sudo apt-get install festival
-sudo apt-get install pocketsphinx-utils
-sudo apt-get install libjack0 libjack-dev
-sudo apt-get install portaudio19-dev
-sudo apt-get install python-all-dev
-sudo apt-get install pip
-sudo -s pip install SpeechRecognition
-sudo -s pip install -U nltk
+apt-get install festival
+apt-get install pocketsphinx-utils
+apt-get install libjack0 libjack-dev
+apt-get install portaudio19-dev
+apt-get install python-all-dev
+apt-get install pip
+pip install SpeechRecognition
+pip install -U nltk
 python <<END
 import nltk 
 nltk.download('all')
 END
-sudo -s pip install bs4
-sudo -s pip install wolframalpha
-sudo pip install pyaudio
-sudo apt-get install git
+pip install bs4
+pip install wolframalpha
+install pyaudio
+apt-get install git
 
 echo "Downloading alan from github"
 git clone https://github.com/camtaylor/alan.git
